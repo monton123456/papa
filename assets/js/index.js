7f35000a205fa04602c3a0a781ae40d3d0a047bf
monton123456 <130204974+monton123456@users.noreply.github.com>
2024-03-26 01:37:13 +1000

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove' , (e)=>{
     Object.assign(document.documentElement, {
-        style: `-moveX: ${e.clientX - window.innerWight / 2}deg; --moveY: ${
-            e.clientX - window.innerHeight / 2
-        }deg`
+                style: `
+        --moveX: ${e.clientX - window.innerWight / 2}deg; 
+         --moveY: ${e.clientX - window.innerHeight / 2}deg;
+        `
     })
-
 })
